21eb96a5352051a2d0acda52deaf91119f3bf967
Ahmad Salah <you@example.com>
2023-08-10 00:01:41 +0300

    updat filles

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+      <>
+      <h1>hello,,,</h1>
+        <h1 className="title">Ahmad salah</h1>
+        <h2>Ahmad salah</h2>
+      </>
+  );
+}
+export default App;
